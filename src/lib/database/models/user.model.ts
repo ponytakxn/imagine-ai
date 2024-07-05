4c4e2dcdeb0c2bbd21254b6564f2ace4e105012a
pony-v <juan.valdes@applydigital.com>
2024-07-05 18:04:13 -0400

    implement clerk webhooks

--- src/lib/database/models/user.model.ts
+++ src/lib/database/models/user.model.ts
@@ -2,8 +2,8 @@ import { Schema, model, models } from "mongoose";
 
 export interface IUser extends Document {
   clerkId: string,
-  firstName?: string,
-  lastName?: string,
+  firstName: string,
+  lastName: string,
   email: string,
   username: string,
   photo: string,
@@ -13,10 +13,10 @@ export interface IUser extends Document {
 
 const UserSchema = new Schema({
   clerkId: { type: String, required: true, unique: true},
-  firstName: { type: String, required: false },
-  lastName: { type: String, required: false },
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
   email: { type: String, required: true, unique: true},
-  username: { type: String, required: false, unique: true},
+  username: { type: String, required: true, unique: true},
   photo: { type: String, required: true },
   planId: { type: Number, default: 1 },
   creditBalance: { type: Number, default: 10 },
