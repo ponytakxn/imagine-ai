2024743bac60b835620c515b35e2d7106c686959
pony-v <juan.valdes@applydigital.com>
2024-07-05 14:23:15 -0400

    fix transformation page export

--- src/app/(root)/transformations/[id]/update/page.tsx
+++ src/app/(root)/transformations/[id]/update/page.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
 
-export const UpdateTransformationPage = () => {
+function UpdateTransformationPage() {
   return (
     <div>UpdateTransformationPage</div>
   )
 }
+
+export default UpdateTransformationPage
